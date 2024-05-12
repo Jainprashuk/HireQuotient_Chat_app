8b46c5116d8f363b2a1acc1db60fd1f4240a5793
prashuk jain <29jainprashuk@gmail.com>
2024-05-12 20:59:32 +0530

    Starter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
@@ -6,24 +7,47 @@ import SignUp from "./pages/signup/SignUp";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
 import Header from "./components/Header/Header";
-
+import Starter from "./components/Starter";
 
 function App() {
-	const { authUser } = useAuthContext();
-	return (
-		<>
-<Header/>
-<div className='p-4 h-screen flex items-center justify-center bg-gradient-to-r from-rose-100 to-teal-100'>
-			<Routes>
-				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
-				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
-				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
-			</Routes>
-			<Toaster />
-		</div>
-		</>
-		
-	);
-}
+  const { authUser } = useAuthContext();
+  const [showStarter, setShowStarter] = useState(true);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowStarter(false);
+    }, 5000); // 5000 milliseconds = 5 seconds
+
+    return () => clearTimeout(timer); // Cleanup timer on unmount
+  }, []);
+
+  return (
+    <>
+      {showStarter ? (
+        <Starter /> // Render Starter component for the first 5 seconds
+      ) : (
+        <>
+          <Header />
+          <div className='p-4 h-screen flex items-center justify-center bg-gradient-to-r from-rose-100 to-teal-100'>
+            <Routes>
+              <Route
+                path='/'
+                element={authUser ? <Home /> : <Navigate to={"/login"} />}
+              />
+              <Route
+                path='/login'
+                element={authUser ? <Navigate to='/' /> : <Login />}
+              />
+              <Route
+                path='/signup'
+                element={authUser ? <Navigate to='/' /> : <SignUp />}
+              />
+            </Routes>
+            <Toaster />
+          </div>
+        </>
+      )}
+    </>
+  );
+}
 export default App;
